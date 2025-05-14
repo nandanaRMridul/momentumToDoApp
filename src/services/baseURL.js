862a1f3024c6f8022447f1daa5134bf087112001
Nandana R <nandana.rm99@gmail.com>
2025-05-14 16:14:12 +0530

    changes: baseURL

--- src/services/baseURL.js
+++ src/services/baseURL.js
@@ -1,2 +1,2 @@
-/* export const baseURL = "https://mserver-jpls.onrender.com"; */
-export const baseURL = "http://localhost:3000";
+export const baseURL = "https://mserver-jpls.onrender.com";
+/* export const baseURL = "http://localhost:3000"; */
